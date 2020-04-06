4808919d25049bfb988c23c7f2896c05597a00fa
Luigi Zuccarelli <lzuccarelli@localhost.localdomain>
2020-04-06 10:15:59 +0100

    Updated for composite testing

--- handlers.go
+++ handlers.go
@@ -3,19 +3,54 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+
+	"github.com/microlib/simple"
 )
 
 type Response struct {
-	Name   string `json:"name"`
-	Email  string `json:"email"`
-	Status string `json:"status"`
+	Status     string `json:"status"`
+	StatusCode string `json:"statuscode"`
+	Message    string `json:"message"`
+	//Payload []Data
+	Result string `json:"result"`
 }
 
-func SimpleHandler(w http.ResponseWriter, r *http.Request) {
-	var response Response
-	response = Response{Status: "200", Name: "Napoleon", Email: "napoleon@dynamite.co"}
+type CompositeResponse struct {
+	Payload string `json:"payload"`
+}
 
+type Data struct {
+	Type      string      `json:"type"`
+	Model     string      `json:"model"`
+	Mac       string      `json:"mac"`
+	DeviceMac string      `json:"devicemac"`
+	Rssi      json.Number `json:"rssi"`
+	Diastolic json.Number `json:"diastolic"`
+	Systolic  json.Number `json:"systolic"`
+	Oxygen    json.Number `json:"oxygen"`
+	Static    json.Number `json:"staticheartrate"`
+	HeartRate json.Number `json:"heartrate"`
+	Battery   json.Number `json:"battery"`
+	Count     json.Number `json:"count"`
+}
+
+func SimpleHandler(w http.ResponseWriter, r *http.Request, logger *simple.Logger) {
+	var response *Response
+	// data, _ := RedisClient.Get("timeseriesdata").Result()
+	body, err := ioutil.ReadAll(r.Body)
+	logger.Debug(fmt.Sprintf("Input data %s", string(body)))
+	if err != nil {
+		logger.Error(fmt.Sprintf("could not read body data %v", err))
+		response = &Response{StatusCode: "500", Status: "KO", Message: fmt.Sprintf("Could not read body data %v", err), Result: ""}
+		w.WriteHeader(http.StatusInternalServerError)
+	} else {
+		//json.Unmarshal([]byte(body), &result)
+		//ilogger.Debug(fmt.Sprintf("Payload data %s", result.Payload))
+		response = &Response{Status: "OK", StatusCode: "200", Message: "Request Successfulli", Result: string(body)}
+		w.WriteHeader(http.StatusOK)
+	}
 	b, _ := json.MarshalIndent(response, "", "	")
 	fmt.Fprintf(w, string(b))
 }
--- simple-web-service.go
+++ simple-web-service.go
@@ -5,17 +5,22 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+
+	"github.com/microlib/simple"
+	// "github.com/go-redis/redis"
 )
 
 var (
-	logger Logger
+	logger *simple.Logger
+	//RedisClient *redis.Client
 )
 
-func startHttpServer(port string) *http.Server {
-	logger.Level = "trace"
+func startHttpServer(port string, logger *simple.Logger) *http.Server {
 	srv := &http.Server{Addr: ":" + port}
 
-	http.HandleFunc("/", SimpleHandler)
+	http.HandleFunc("/api/v1/service", func(w http.ResponseWriter, r *http.Request) {
+		SimpleHandler(w, r, logger)
+	})
 	http.HandleFunc("/isalive", IsAlive)
 
 	go func() {
@@ -28,7 +33,23 @@ func startHttpServer(port string) *http.Server {
 }
 
 func main() {
-	srv := startHttpServer("9000")
+
+	// connect to redis
+	/*
+		RedisClient = redis.NewClient(&redis.Options{
+			Addr:         "127.0.0.1:6379",
+			DialTimeout:  10 * time.Second,
+			ReadTimeout:  30 * time.Second,
+			WriteTimeout: 30 * time.Second,
+			PoolSize:     10,
+			PoolTimeout:  30 * time.Second,
+			Password:     "",
+			DB:           0,
+		})
+	*/
+	logger = &simple.Logger{Level: "trace"}
+
+	srv := startHttpServer(os.Args[1], logger)
 	logger.Info("Starting server on port " + srv.Addr)
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
